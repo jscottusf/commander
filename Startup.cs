61cf2afaa0fd4ff619f9e5ccabeed7ec397e1184
Joel Scott <jscottusf@gmail.com>
2020-05-30 21:57:11 -0400

    added patch route

--- Startup.cs
+++ Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 
 //create a migration file
 //dotnet ef migrations add InitialMigrations
@@ -33,7 +34,10 @@ namespace Commander
             services.AddDbContext<CommanderContext>(opt => opt.UseSqlServer
             (Configuration.GetConnectionString("CommanderConnection")));
 
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(s =>
+            {
+                s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            });
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
